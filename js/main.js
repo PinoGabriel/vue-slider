3520814f2265e41281fac04b9b9e95efb120d9d0
PinoGabriel <pinogabriel01@gmail.com>
2023-11-16 17:28:08 +0100

    feat: added miniature inside img container

--- js/main.js
+++ js/main.js
@@ -1,35 +1,35 @@
 const { createApp } = Vue
 
-  createApp({
+createApp({
     data() {
-      return {
-        slides: [
-            {
-                immagine: 'https://img.freepik.com/free-psd/dark-mobile-device-mockup_149660-787.jpg',
-                nome: 'Smartphone XYZ',
-                descrizione: 'Uno smartphone potente con caratteristiche avanzate e display nitido.',
-                prezzo: 499.99
-            },
-            {
-                immagine: 'https://img.freepik.com/free-psd/laptop-blue-background-mock-up_1022-178.jpg',
-                nome: 'Laptop ABC',
-                descrizione: 'Un laptop leggero e performante, ideale per la produttività in movimento.',
-                prezzo: 899.99
-            },
-            {
-                immagine: 'https://img.freepik.com/free-photo/photo-camera-balancing-with-yellow-background_23-2150271772.jpg',
-                nome: 'Fotocamera 123',
-                descrizione: 'Una fotocamera professionale con alta risoluzione e molte funzionalità.',
-                prezzo: 699.99
-            }
-        ],
-        slideStart: 0
-      }
+        return {
+            slides: [
+                {
+                    immagine: 'https://img.freepik.com/free-psd/dark-mobile-device-mockup_149660-787.jpg',
+                    nome: 'Smartphone XYZ',
+                    descrizione: 'Uno smartphone potente con caratteristiche avanzate e display nitido.',
+                    prezzo: 499.99
+                },
+                {
+                    immagine: 'https://img.freepik.com/free-psd/laptop-blue-background-mock-up_1022-178.jpg',
+                    nome: 'Laptop ABC',
+                    descrizione: 'Un laptop leggero e performante, ideale per la produttività in movimento.',
+                    prezzo: 899.99
+                },
+                {
+                    immagine: 'https://img.freepik.com/free-photo/photo-camera-balancing-with-yellow-background_23-2150271772.jpg',
+                    nome: 'Fotocamera 123',
+                    descrizione: 'Una fotocamera professionale con alta risoluzione e molte funzionalità.',
+                    prezzo: 699.99
+                }
+            ],
+            slideStart: 0
+        }
     },
 
     methods: {
         fCaroselAvanti() {
-            if (this.slideStart < this.slides.length -1) {
+            if (this.slideStart < this.slides.length - 1) {
                 this.slideStart++
             } else {
                 this.slideStart = 0
@@ -39,12 +39,12 @@ const { createApp } = Vue
             if (this.slideStart > 0) {
                 this.slideStart--
             } else {
-                this.slideStart = this.slides.length -1
+                this.slideStart = this.slides.length - 1
             }
-          },
-          selectSlide(index) {
+        },
+        selectSlide(index) {
             this.slideStart = (index)
-          }
+        }
     }
 
 
