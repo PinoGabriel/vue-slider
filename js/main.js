dae38ea1dd0cda1a0b67c208bc99f96676b23fac
PinoGabriel <pinogabriel01@gmail.com>
2023-11-16 16:06:08 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        slides: [
+            {
+                immagine: 'https://img.freepik.com/free-psd/dark-mobile-device-mockup_149660-787.jpg',
+                nome: 'Smartphone XYZ',
+                descrizione: 'Uno smartphone potente con caratteristiche avanzate e display nitido.',
+                prezzo: 499.99
+            },
+            {
+                immagine: 'https://img.freepik.com/free-psd/laptop-blue-background-mock-up_1022-178.jpg',
+                nome: 'Laptop ABC',
+                descrizione: 'Un laptop leggero e performante, ideale per la produttività in movimento.',
+                prezzo: 899.99
+            },
+            {
+                immagine: 'https://img.freepik.com/free-photo/photo-camera-balancing-with-yellow-background_23-2150271772.jpg',
+                nome: 'Fotocamera 123',
+                descrizione: 'Una fotocamera professionale con alta risoluzione e molte funzionalità.',
+                prezzo: 699.99
+            }
+        ],
+        slideStart: 0
+      }
+    },
+
+    methods: {
+        fCaroselAvanti() {
+            if (this.slideStart < this.slides.length -1) {
+                this.slideStart++
+            } else {
+                this.slideStart = 0
+            }
+        },
+        fCaroselIndietro() {
+            if (this.slideStart > 0) {
+                this.slideStart--
+            } else {
+                this.slideStart = this.slides.length -1
+            }
+          }
+    }
+
+
+}).mount('#app')
